b21f73f6bcf774dda4c83eefaaa9f3dc481e748a
Naresh0979 <nareshkumawat0979@gmail.com>
2025-04-03 00:59:23 +0530

    config added

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,20 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "flickfuse-1abca.firebaseapp.com",
-  projectId: "flickfuse-1abca",
-  storageBucket: "flickfuse-1abca.appspot.com",
-  messagingSenderId: "440612446081",
-  appId: "1:440612446081:web:e754d9e4b30026669a9280",
-  measurementId: "G-NJTMG82R26",
+  authDomain: "flick-fuse.firebaseapp.com",
+  projectId: "flick-fuse",
+  storageBucket: "flick-fuse.firebasestorage.app",
+  messagingSenderId: "670722670892",
+  appId: "1:670722670892:web:6ac50cde2c75573c806c89"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
